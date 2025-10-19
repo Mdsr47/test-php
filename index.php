c793575abdfaa1416acf6c48814d3648b6aa3141
SANDHU <mdsr1850@gmail.com>
2025-10-19 16:50:07 +0500

    again testt

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 echo "🎉 Hello from GitHub Actions + InfinityFree!";
 echo "sandhu"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAndhu</title>
+</head>
+<body>
+    <h1>Welcome ?sandhu?</h1>
+    <h6>first website live with ci/cd pipeline</h6>
+</body>
+</html>
